4b14fee16258a169ffa3520f7fd724510502806f
y604499781 <54754052+y604499781@users.noreply.github.com>
2020-03-04 19:14:49 -0500

    ice10

--- quizie/src/App.js
+++ quizie/src/App.js
@@ -5,6 +5,7 @@ import questions from './questions.json'
 const TITLE_STATE = 0
 const QUESTION_STATE = 1
 const TIME_LIMIT = 5
+const FINAL_STATE = 2
 
 
 class QuizQuestion extends React.Component {
@@ -21,7 +22,7 @@ class TitlePage extends React.Component{
   constructor(props){
     super(props)
     this.state ={
-      titleText: "Welcome to our Quiz!",
+      titleText: "Welcome to Quizie!!!",
       counter: 0,
       currentState: TITLE_STATE,
       currentQuestion: 0,
@@ -31,18 +32,22 @@ class TitlePage extends React.Component{
   }
   nextQuestion(correct){
     console.log("BUTTON PRESSED")
+    this.timer = setInterval(() => this.countdown(), 1000)
     if(correct){
       this.setState({score: this.state.score+1})
     }
     if(this.state.currentQuestion === questions.length -1){
       console.log("DONE")
-    }
-    else{
-      clearInterval(this.timer)
-      console.log(this.state.currentQuestion)
-      
       this.setState({
-        titleText:"You answers X",
+        titleText: 'Restart Quiz',
+        currentState: FINAL_STATE,
+        score: this.state.score
+      })
+    } else{
+      clearInterval(this.timer)
+      console.log(this.state.currentQuestion)  
+      this.setState({
+        titleText:"You answer X",
         currentState: QUESTION_STATE,
         currentQuestion:this.state.currentQuestion +1
       })
@@ -53,7 +58,7 @@ class TitlePage extends React.Component{
     console.log(this.state.counter)
     if(this.state.counter < this.timeLimit){
       this.setState({
-        titleText: 'Starting the Quiz',
+        titleText: 'Starting the Quiz!',
         counter: this.state.counter +1
       })
     }else{
